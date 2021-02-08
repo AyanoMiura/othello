9d6f545d960328ee85cc58976fda3045ed63f9e0
AyanoMiura <69710221+AyanoMiura@users.noreply.github.com>
2021-02-08 23:13:08 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,62 @@
 //オセロ
+//オセロ
 var empty=0;
 var black=1;
 var white=2;
 var cells=[];
-
+var stone;
 //盤のidを取得
 var board=document.getElementById('board');
+//盤に表示
 window.onload = function(){
+  //tdにlavelを生成
+  //lavelでマスと盤を関連づける
+  function addelement(){
+    var td=document.querySelectorAll('td');
+    var label=document.createElement('label');
+    //取得したtdのすべてにlavelを追加
+    for(var x=0;x<td.length;++x){
+      td.appendChild(label);
+    }
+   }
 
-
-//初期化
-ban_init();
+  //盤の初期化
+  ban_init();
+  //クリックイベント
+  for(var x=0;x<8;x++){
+    for (var y = 0; y <8; y++){
+      var selec_cell=board.rows[x].cells[y];
+      select_cell.onclick=function(){
+      ban_set();
+      }
+    }
+  }
 
 };
-//tdにlavelを生成
-  function addelement(){
-    var td=board.getElementsByTagName('td');
-    var label=document.createElement('label');
-    td.appendChild(label);
-  };
+
+
 
 //盤の初期化
 function ban_init(){
-  for(var x=0;x<8;x++){
-    for (var y = 0; y <8; y++){
-      cells[x][y];
+  //盤のマスを縦軸と横軸でループ
+    for(var x=0;x<8;x++){
+      cells[x]=[];
+      for (var y = 0; y <8; y++){
+        cells[x][y]=empty;
+      }
     }
-  }
-  //初期配置
-  ban[3][3]=2;
-  ban[3][4]=1;
-  ban[4][3]=1;
-  ban[4][4]=2;
-  ban_set();
-};
+    //初期配置
+    cells[3][3]=2;
+    cells[3][4]=1;
+    cells[4][3]=1;
+    cells[4][4]=2;
+    ban_set();
+}
 
 
 
 // 石を置く
 function ban_set(){
-  var stone;
   for(var x=0;x<8;x++){
     for (var y = 0; y <8; y++){
     switch (cells[x][y]) {
@@ -54,7 +71,7 @@ function ban_set(){
         break;
     }
     //盤の中で行とセルを取得
-    board.row[x].cells[y].innertext=stone;
+    cells[x][y].innertext=stone;
     }
   }
-};
+}
