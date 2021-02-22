e74b41ad5ea1195ffbb9178b307c7d94da667aaf
AyanoMiura <69710221+AyanoMiura@users.noreply.github.com>
2021-02-22 23:04:52 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,100 +1,95 @@
 //オセロ
-//空のマスの変数
 var empty=0;
-//黒の石の変数
-var black=1;
-//白の石の変数
-var white=2;
 //マスの位置を把握する変数
-var cells=[];
-//白黒の石を判断する変数
-var stone;
-//盤を取得する変数
-var board;
-
+var cell=[];
+//1なら黒、-1なら白
+var turn=1;
 //盤に表示
 window.onload = function(){
   //盤のidを取得
-  board=document.getElementById('board');
-
-  //盤の初期化
-  ban_init();
-
-  //8行をループ
+var board=document.getElementById('board');
+  //初期化,すべてのセルに0を入れる
   for(var x=0;x<8;x++){
-    //1行の中の8マスをループ
-    for (var y = 0; y <8; y++){
-      //取得した盤のマスを変数に格納
-      var select_cell=board.rows[x].cells[y];
-      //取得したマスをクリックした時の動き
-      select_cell.onclick=function(){
-      //石を置く
-      ban_set();
-      }
+    board[x]=[];
+    for(var y=0;y<8;y++){
+      board[x][y]=empty;
     }
   }
+  //tableの全てにクリックイベントを付与
+  //盤の行をループ
+  for (var x=0;x<board.rows.length;x++){
+    //x行のマスをループ
+    for(var y=0;y<board.rows[x].cells.length;y++){
+       //取得したセルを変数に格納
+       var squares=board.rows[x].cells[y];
 
+       //クリックイベント
+       squares.onclick=function(){
+        //石が置いてないか確認
+         if(this.classList.contains('black')==false&&this.classList.contains('white')==false){
+           put_stone(this);
+         }else {
+           alert('そこには置けません')
+         }
+       }
+     }
+  }
+
+  //石を置く処理 numが裏替すコマ
+  function setcell(x,y,num){
+    for(var x=0;x<8;x++){
+      for(var y=0;y<8;y++){
+         cell[x][y]=num;
+      }
+    }
+  };
+  //指定した方向に裏返せるか検索
+  function search_reverse(x,y,add_x,add_y){
+    var xx=x;
+    var yy=y;
+    //裏返した数
+    var koma=0;
+    //自分の石か空のセル以外をtrue,相手の石の時のみfalse
+    var hit=false;
+    while (!hit) {
+      xx+=add_x;
+      yy+=add_y;
+      //進むごとにkomaを足していき、足した分返す
+      koma++;
+      //盤外になったらループを抜ける
+      if(xx<0 || xx>8 || yy<0 || yy>8){
+        break;
+      }
+      //移動先のセルに石がないなら抜ける
+      if (cell[xx][yy]==empty) {
+        break;
+      }
+      //移動先に自分の石があったらmtselfを1にして抜ける。
+      if(cell[xx][yy]==turn){
+        hit=true;
+        //komaをループしてる時に裏返す
+        for(var i=0;i<koma;i++){
+          setcell(xx,yy,koma);
+        }
+      }
+    }
+  };
+
+//石を置く
+  function put_stone(a){
+    for(var x=0;x<8;x++){
+      cell[x]=[];
+      for(var y=0;y<8;y++){
+        cell[x][y];
+      }
+    }
+    if(turn==1){
+      a.classList.add('black');
+      search_reverse(x,y,1,0);//右に調べる
+      turn=turn*-1;
+    }else{
+      a.classList.add('white');
+      turn=turn*-1;
+    }
+  };
 };
-
-//tdにlavelを生成する関数
-//lavelでマスと盤を関連づける
-  function addelement(){
-    //すべてのtdを取得し変数に格納
-    var td=document.querySelectorAll('td');
-    //labelを生成
-    var label=document.createElement('label');
-    //取得したtdのすべてにlavelを追加
-    for(var x=0;x<td.length;++x){
-       td.appendChild(label);
-    }
-  }
-
-//盤の初期化する関数
-function ban_init(){
-  //8行をループ
-  for(var x=0;x<8;x++){
-  //何行目かを取得
-    cells[x]=[];
-    //x行の中で8マスをループ
-    for (var y = 0; y <8; y++){
-        //x行目のｙマスを取得し空のマスを代入
-        cells[x][y]=0;
-      }
-    }
-    //初期配置
-    cells[3][3]=2;
-    cells[3][4]=1;
-    cells[4][3]=1;
-    cells[4][4]=2;
-    //石を置く
-    ban_set();
-}
-
-
-
-// 石を置く関数
-function ban_set(){
-  //8行をループ
-  for(var x=0;x<8;x++){
-    //x行目の中で8マスループ
-    for (var y = 0; y <8; y++){
-    //x行目のyマスが白黒のどちらか判断
-    switch (cells[x][y]) {
-      //0の場合、空
-      case 0:
-        stone="";
-        break;
-      //1の場合、黒
-      case 1:
-        stone="●"
-        break;
-      //2の場合、白
-      case 2:
-        stone="○";
-        break;
-    }
-    //盤のマスに取得し合致した石を表示
-    board.rows[x].cells[y].textcontent=stone;
-    }
-  }
-}
