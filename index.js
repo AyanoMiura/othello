3dd53c228bc30a31e891d45e903ffce6d8452b52
AyanoMiura <69710221+AyanoMiura@users.noreply.github.com>
2021-03-30 14:31:16 +0900

    Update index.js

--- index.js
+++ index.js
@@ -8,51 +8,62 @@ imgPath[WHITE_TURN] = "game_reversi_white.png"; // 白
 // 現在のターンを示す変数
 var turn=BLACK_TURN;
 var board_data=[
-  [9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
-  [9, 0, 0, 0, 0, 0, 0, 0, 0, 9],
-  [9, 0, 0, 0, 0, 0, 0, 0, 0, 9],
-  [9, 0, 0, 0, 0, 0, 0, 0, 0, 9],
-  [9, 0, 0, 0, -1, 1, 0, 0, 0, 9],
-  [9, 0, 0, 0, 1, -1, 0, 0, 0, 9],
-  [9, 0, 0, 0, 0, 0, 0, 0, 0, 9],
-  [9, 0, 0, 0, 0, 0, 0, 0, 0, 9],
-  [9, 0, 0, 0, 0, 0, 0, 0, 0, 9],
-  [9, 9, 9, 9, 9, 9, 9, 9, 9, 9]
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, -1, 1, 0, 0, 0],
+  [0, 0, 0, 1, -1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
 ];
 var coordinate=[[1,0],[0,1],[1,1],[1,-1],[0,-1],[-1,0],[-1,-1],[-1,1]];
 
 
 window.onload=function(){
+  ban_init();
   var board=document.getElementById("board");
   for(var x=0;x<8;x++){
     for(var y=0;y<8;y++){
       var select_cell=board.rows[x].cells[y];
       select_cell.onclick=function(){
         //クリックした時の動作
-         check_reverse(this.parentNode.rowIndex,this.cellIndex,turn);
+       check_reverse(this.parentNode.rowIndex,this.cellIndex,turn);
          ban_set(this);
          chenge_turn();
+
      }
     }
   }
 };//onloadの終わり
 
-function ban_set(cell){
-  var piece_img=document.createElement('img');
-  for (var x= 1;x < 9; x++) {
+//盤の初期化
+function ban_init(){
+  for (var x= 0;x <= 7; x++) {
        board_data[x] = [];
-       for (var y = 1; y < 9; y++) {
+       for (var y = 0; y <= 7; y++) {
            board_data[x][y] =0;
        }
    }
+   //全てのセルにidを振る
+for(var x = 0; x< board.rows.length;x++){
+ for(var y = 0; y < board.rows[x].cells.length;y++){
+  board.rows[x].cells[y].id = "p" + x + y;
+  }
+}
+   board_data[3][3]=-1;
+   board_data[3][4]=1;
    board_data[4][4]=-1;
-   board_data[4][5]=1;
-   board_data[5][5]=-1;
-   board_data[5][4]=1;
+   board_data[4][3]=1;
+}
 
 
-  for (var x= 1;x < 9; x++) {
-    for (var y = 1; y < 9; y++) {
+
+//駒を置く
+function ban_set(cell){
+  var piece_img=document.createElement('img');
+  for (var x= 0;x < 8; x++) {
+    for (var y = 0; y < 8; y++) {
         if( board_data[x][y]==1){
   　     piece_img.src=imgPath[turn];
         }else if( board_data[x][y]==-1){
@@ -72,18 +83,17 @@ function get_opponent(turn){
   return turn*-1;
 }
 
+
 function stone_reverse(x,y,turn){
- var img=document.getElementsByTagName('img');
- if (turn==1) {
-   img.src=imgPath[turn];
- }else if (turn==-1) {
-   img.src=imgPath[turn];
- }
+  var cell=document.getElementById("p" + x + y);
+  var img =cell.lastElementChild;
+  img.src=imgPath[turn];
 }
 
 
 //駒が置けるかチェック、置けたときの処理
 function check_reverse(x,y,turn){
+
   var opponent=get_opponent(turn);
   var koma_cnt=0;
   var hit=false;
@@ -94,35 +104,36 @@ function check_reverse(x,y,turn){
     var xx=x+cx;
     var yy=y+cy;
 
-    if (board_data[xx][yy]!=opponent ){  //相手の駒がなければ以降の処理をしない
-      continue;
-    }
-
-    while (board_data[xx][yy]==opponent) { //相手の駒がある間はループする
-      xx+=cx;
-      yy+=cy;
-      koma_cnt++;
-      if(board_data[xx][yy]==0){ //移動先に石がないなら抜ける
-        hit = false;
-        break;
-      }else if(xx==9 || yy==9) { //盤外なら抜ける
-        hit = false;
-        break;
-      }else if (board_data[xx][yy]==turn) { //移動先に自分の石がある時、裏返す
-        hit=true;
+    while (true) { //相手の駒がある間はループする
+      if (xx < 0 || xx > 7 || yy < 0 || yy > 7) { //盤外
         break;
       }
-     }
-   }
-   if (hit==true) {
-     var px=xx-cx;
-     var py=yy-cy;
-     for (var a = 0; a < koma_cnt.length; a++) { //駒を数えた分ループする
-       board_data[px][py]=turn;
-       stone_reverse(px,py,turn);
-       px-=cx;
-       py-=cy;
-       return true;
-   }
- }
+      if(board_data[xx][yy]==0){ //移動先に石がないなら抜ける
+        break;
+      }
+      if(board_data[xx][yy]==opponent) { //相手の駒がある
+        xx+=cx;
+        yy+=cy;
+        koma_cnt++;
+      }
+      if(koma_cnt==0){
+       if(board_data[xx][yy]==turn) { //移動先の1つ目が自分の石なら、何もせず抜ける
+         hit=true;
+         break;
+       }
+      }
+      if (koma_cnt>0) {
+        if (board_data[xx][yy]==turn) {　//移動先の1つ目以上が自分の石なら、裏返す
+          hit=true;
+          var px=xx;
+          var py=yy;
+          for (var a = 0; a < koma_cnt; a++) { //駒を数えた分ループする
+            px-=cx;
+            py-=cy;
+            stone_reverse(px,py,turn);
+          }
+        }
+      }
+    }
+  }
 }
